f3d4857efae7c953cde69ef3818ba875ab274b1f
Omar Villafuerte <ovillafuerte.94@gmail.com>
2021-04-13 03:03:13 -0400

    ProfileTest created

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,4 +9,11 @@ Route::get('/', function () {
 
 Route::get('about', function () {
     return 'Sesión acerca de';
+});
+
+Route::view('profile', 'profile');
+
+Route::post('profile', function (Request $request) {
+    $request->file('photo')->store('profile');
+    return redirect('profile');
 });
---
+++ tests/Feature/ProfileTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+
+class ProfileTest extends TestCase
+{
+    public function test_upload()
+    {
+        Storage::fake('local');
+
+        $response = $this->post('profile', [
+            'photo' => UploadedFile::fake()->image('foto.png')
+        ]);
+
+        $response->assertRedirect('profile');
+    }
+}
